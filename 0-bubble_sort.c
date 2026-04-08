de0e56a8f09a7494dea8cf8a067d9d09c4c01c45
Chihaan <you@example.com>
2026-04-08 12:32:47 +0200

    Add : File 0 & 1 of sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,5 +11,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int tmp, swap;
 
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			swap = 0;
+			if (array[j + 1] < array[j])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swap)
+			break;
+	}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,5 +10,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *check, *tmp, *swap;
 
+	if (!list)
+		return;
+
+	check = *list;
+	check = check->next;
+
+	while (check)
+	{
+		tmp = check->next;
+		while (check->prev && check->prev->n > check->n)
+		{
+			swap = check->prev;
+			swap->next = check->next;
+			check->next = swap;
+			check->prev = swap->prev;
+			swap->prev = check;
+			if (swap->next)
+				swap->next->prev = swap;
+			if (check->prev)
+				check->prev->next = check;
+			else
+				*list = check;
+			print_list(*list);
+		}
+		check = tmp;
+	}
 }
