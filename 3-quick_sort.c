6874dc2c41b30c186a95557215741cc27df718b3
Chihaan <you@example.com>
2026-04-08 16:47:47 +0200

    Added : File 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,6 +2,59 @@
 #include <stdlib.h>
 #include "sort.h"
 /**
+ * partition - Swaps boxes.
+ *
+ * @array: The array.
+ * @size: Size of the array.
+ * @low: The lowest idx.
+ * @high: The highest idx.
+ *
+ * Return: Nothing.
+ */
+int partition(int *array, size_t size, int low, int high)
+{
+	int pivot = array[high], count = low, j, tmp;
+	(void)size;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			tmp = array[count];
+			array[count] = array[j];
+			array[j] = tmp;
+			count++;
+		}
+	}
+	tmp = array[count];
+	array[count] = array[high];
+	array[high] = tmp;
+	print_array(array, size);
+	return (count);
+}
+/**
+ * quick_sort_recursion - Sorts an array using recursion.
+ *
+ * @array: The array.
+ * @size: Size of the array.
+ * @low: The lowest idx.
+ * @high: The highest idx.
+ *
+ * Return: Nothing.
+ */
+void quick_sort_recursion(int *array, size_t size, int low, int high)
+{
+	int pivot_idx;
+
+	if (low >= high)
+		return;
+
+	pivot_idx = partition(array, size, low, high);
+
+	quick_sort_recursion(array, size, low, pivot_idx - 1);
+	quick_sort_recursion(array, size, pivot_idx + 1, high);
+}
+/**
  * quick_sort - Sorts an array of integers in ascending order.
  *
  * @array: The array.
@@ -11,5 +64,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-
+	if (!array || size < 2)
+		return;
+	quick_sort_recursion(array, size, 0, size - 1);
 }
